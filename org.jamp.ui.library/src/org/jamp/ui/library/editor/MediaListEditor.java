81c72ef13505278bb440c7db1f497637eb1a394a
eric <eric@jamp.com>
2008-07-24 17:29:26 +0000

    adding media listeditorinput

---
+++ org.jamp.ui.library/src/org/jamp/ui/library/editor/MediaListEditor.java
@@ -0,0 +1,93 @@
+package org.jamp.ui.library.editor;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+
+public class MediaListEditor extends EditorPart {
+	
+	public final static String ID = "org.jamp.ui.library.editor.MediaList";
+
+	public MediaListEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		setSite(site);
+		setInput(input);
+		setPartName(input.getName());
+	}
+
+	@Override
+	public boolean isDirty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		GridLayout gridLayout = new GridLayout();
+	    parent.setLayout(gridLayout);
+	    
+	    // Table. 
+	    Table table = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION);
+
+	    //table.setLinesVisible(true);
+	    table.setHeaderVisible(true);
+	    
+	    TableColumn tcFileName = new TableColumn(table, SWT.LEFT);
+	    tcFileName.setText("File name");
+	    
+	    TableColumn tcFileSize = new TableColumn(table, SWT.RIGHT);
+	    tcFileSize.setText("Size");
+	    
+	    TableColumn tcDateModified = new TableColumn(table, SWT.RIGHT);
+	    tcDateModified.setText("Date Modified");
+	    
+	    tcFileName.setWidth(200);
+	    tcFileSize.setWidth(80);
+	    tcDateModified.setWidth(180);
+	    
+	    TableItem item = new TableItem(table, SWT.NULL);
+	    item.setText(new String[]{"Name", "Size"});
+	    
+	    table.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ org.jamp.ui.library/src/org/jamp/ui/library/editor/MediaListEditorInput.java
@@ -0,0 +1,69 @@
+package org.jamp.ui.library.editor;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPersistableElement;
+
+public class MediaListEditorInput implements IEditorInput {
+
+	private final String _playList;
+
+	MediaListEditorInput(String playListName) {
+		Assert.isNotNull(playListName);
+		_playList = playListName;
+	}
+
+	@Override
+	public boolean exists() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ImageDescriptor getImageDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistableElement getPersistable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getToolTipText() {
+		return _playList;
+	}
+
+	@Override
+	public Object getAdapter(Class adapter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (super.equals(obj)) {
+			return true;
+		}
+		if (!(obj instanceof MediaListEditorInput)) {
+			return false;
+		}
+		MediaListEditorInput other = (MediaListEditorInput) obj;
+		return _playList.equals(other._playList);
+	}
+
+	@Override
+	public int hashCode() {
+		return _playList.hashCode();
+	}
+
+}
