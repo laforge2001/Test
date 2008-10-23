8310d9de85da2201f8e6b7164c397a7294f3e646
eric <eric@jamp.com>
2008-10-23 16:51:21 +0000

    commenting out player buttons view

--- org.jamp.ui/src/org/jamp/ui/Perspective.java
+++ org.jamp.ui/src/org/jamp/ui/Perspective.java
@@ -3,7 +3,6 @@ package org.jamp.ui;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.jamp.ui.library.views.LibraryView;
-import org.jamp.ui.library.views.PlayerView;
 
 public class Perspective implements IPerspectiveFactory {
 
@@ -14,7 +13,7 @@ public class Perspective implements IPerspectiveFactory {
 
 		layout.addStandaloneView(LibraryView.ID, true, IPageLayout.LEFT, 0.3f,
 				editorArea);
-		layout.addStandaloneView(PlayerView.ID, false, IPageLayout.BOTTOM,
-				0.8f, editorArea);
+		// layout.addStandaloneView(PlayerView.ID, false, IPageLayout.BOTTOM,
+		// 0.8f, editorArea);
 	}
 }
