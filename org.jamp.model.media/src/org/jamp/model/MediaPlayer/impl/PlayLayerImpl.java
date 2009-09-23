cc89c9f15011e98a07b428935721ad6c5cd312ff
eric <eric@jamp.com>
2009-09-23 12:44:51 +0000

    adding play layer service

--- org.jamp.model.media/src/org/jamp/model/MediaPlayer/impl/PlayLayerImpl.java
+++ org.jamp.model.media/src/org/jamp/model/MediaPlayer/impl/PlayLayerImpl.java
@@ -9,41 +9,38 @@ package org.jamp.model.MediaPlayer.impl;
 import java.util.Collection;
 
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
-
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
-
 import org.eclipse.emf.ecore.util.EObjectResolvingEList;
-
 import org.jamp.model.MediaPlayer.Library;
 import org.jamp.model.MediaPlayer.MediaApi;
 import org.jamp.model.MediaPlayer.MediaPlayerPackage;
 import org.jamp.model.MediaPlayer.PlayLayer;
 
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Play Layer</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> An implementation of the model object '
+ * <em><b>Play Layer</b></em>'. <!-- end-user-doc -->
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link org.jamp.model.MediaPlayer.impl.PlayLayerImpl#getInstalledApi <em>Installed Api</em>}</li>
- *   <li>{@link org.jamp.model.MediaPlayer.impl.PlayLayerImpl#getMyLibrary <em>My Library</em>}</li>
+ * <li>{@link org.jamp.model.MediaPlayer.impl.PlayLayerImpl#getInstalledApi <em>
+ * Installed Api</em>}</li>
+ * <li>{@link org.jamp.model.MediaPlayer.impl.PlayLayerImpl#getMyLibrary <em>My
+ * Library</em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @generated
  */
 public class PlayLayerImpl extends EObjectImpl implements PlayLayer {
 	/**
-	 * The cached value of the '{@link #getInstalledApi() <em>Installed Api</em>}' reference list.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The cached value of the '{@link #getInstalledApi()
+	 * <em>Installed Api</em>}' reference list. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @see #getInstalledApi()
 	 * @generated
 	 * @ordered
@@ -51,9 +48,9 @@ public class PlayLayerImpl extends EObjectImpl implements PlayLayer {
 	protected EList<MediaApi> installedApi;
 
 	/**
-	 * The cached value of the '{@link #getMyLibrary() <em>My Library</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The cached value of the '{@link #getMyLibrary() <em>My Library</em>}'
+	 * reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getMyLibrary()
 	 * @generated
 	 * @ordered
@@ -61,8 +58,8 @@ public class PlayLayerImpl extends EObjectImpl implements PlayLayer {
 	protected Library myLibrary;
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected PlayLayerImpl() {
@@ -70,8 +67,8 @@ public class PlayLayerImpl extends EObjectImpl implements PlayLayer {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -80,37 +77,40 @@ public class PlayLayerImpl extends EObjectImpl implements PlayLayer {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public EList<MediaApi> getInstalledApi() {
 		if (installedApi == null) {
-			installedApi = new EObjectResolvingEList<MediaApi>(MediaApi.class, this, MediaPlayerPackage.PLAY_LAYER__INSTALLED_API);
+			installedApi = new EObjectResolvingEList<MediaApi>(MediaApi.class,
+					this, MediaPlayerPackage.PLAY_LAYER__INSTALLED_API);
 		}
 		return installedApi;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public Library getMyLibrary() {
 		if (myLibrary != null && myLibrary.eIsProxy()) {
-			InternalEObject oldMyLibrary = (InternalEObject)myLibrary;
-			myLibrary = (Library)eResolveProxy(oldMyLibrary);
+			InternalEObject oldMyLibrary = (InternalEObject) myLibrary;
+			myLibrary = (Library) eResolveProxy(oldMyLibrary);
 			if (myLibrary != oldMyLibrary) {
 				if (eNotificationRequired())
-					eNotify(new ENotificationImpl(this, Notification.RESOLVE, MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY, oldMyLibrary, myLibrary));
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE,
+							MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY,
+							oldMyLibrary, myLibrary));
 			}
 		}
 		return myLibrary;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public Library basicGetMyLibrary() {
@@ -118,108 +118,108 @@ public class PlayLayerImpl extends EObjectImpl implements PlayLayer {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setMyLibrary(Library newMyLibrary) {
 		Library oldMyLibrary = myLibrary;
 		myLibrary = newMyLibrary;
 		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY, oldMyLibrary, myLibrary));
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY, oldMyLibrary,
+					myLibrary));
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * <!-- begin-user-doc --> adds installed codec bundles <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated NOT
 	 */
 	public void registerApi(MediaApi addMe) {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
+		installedApi.add(addMe);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
 	 */
 	public void unregisterApi(MediaApi unregisterMe) {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
+		installedApi.remove(unregisterMe);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
-			case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
-				return getInstalledApi();
-			case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
-				if (resolve) return getMyLibrary();
-				return basicGetMyLibrary();
+		case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
+			return getInstalledApi();
+		case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
+			if (resolve)
+				return getMyLibrary();
+			return basicGetMyLibrary();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
-			case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
-				getInstalledApi().clear();
-				getInstalledApi().addAll((Collection<? extends MediaApi>)newValue);
-				return;
-			case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
-				setMyLibrary((Library)newValue);
-				return;
+		case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
+			getInstalledApi().clear();
+			getInstalledApi().addAll((Collection<? extends MediaApi>) newValue);
+			return;
+		case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
+			setMyLibrary((Library) newValue);
+			return;
 		}
 		super.eSet(featureID, newValue);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
-			case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
-				getInstalledApi().clear();
-				return;
-			case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
-				setMyLibrary((Library)null);
-				return;
+		case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
+			getInstalledApi().clear();
+			return;
+		case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
+			setMyLibrary((Library) null);
+			return;
 		}
 		super.eUnset(featureID);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
-			case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
-				return installedApi != null && !installedApi.isEmpty();
-			case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
-				return myLibrary != null;
+		case MediaPlayerPackage.PLAY_LAYER__INSTALLED_API:
+			return installedApi != null && !installedApi.isEmpty();
+		case MediaPlayerPackage.PLAY_LAYER__MY_LIBRARY:
+			return myLibrary != null;
 		}
 		return super.eIsSet(featureID);
 	}
 
-} //PlayLayerImpl
+} // PlayLayerImpl
