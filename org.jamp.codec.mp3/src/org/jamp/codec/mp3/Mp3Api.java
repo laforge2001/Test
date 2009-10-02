87bf325106c467570e53be69065d592024ac4ff7
eric <eric@jamp.com>
2009-10-02 16:49:41 +0000

    adding source code for jaudiotagger for reference

--- org.jamp.codec.mp3/src/org/jamp/codec/mp3/Mp3Api.java
+++ org.jamp.codec.mp3/src/org/jamp/codec/mp3/Mp3Api.java
@@ -1,5 +1,7 @@
 package org.jamp.codec.mp3;
 
+import java.io.IOException;
+
 import javazoom.jl.player.AudioDevice;
 import javazoom.jl.player.advanced.AdvancedPlayer;
 import javazoom.jl.player.advanced.PlaybackEvent;
@@ -7,11 +9,14 @@ import javazoom.jl.player.advanced.PlaybackEvent;
 import org.jamp.model.MediaPlayer.MediaObject;
 import org.jamp.model.MediaPlayer.State;
 import org.jamp.model.MediaPlayer.impl.MediaApiImpl;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.audio.mp3.MP3File;
 import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
 
 public class Mp3Api extends MediaApiImpl {
 
-	private static Mp3Api _instance;
 	private State _state;
 	private static AdvancedPlayer _player;
 	private AudioDevice _device;
@@ -28,7 +33,7 @@ public class Mp3Api extends MediaApiImpl {
 
 	@Override
 	public boolean canPlay(MediaObject media) {
-		// TODO Auto-generated method stub
+		// _player.
 		return false;
 	}
 
@@ -58,6 +63,31 @@ public class Mp3Api extends MediaApiImpl {
 
 	@Override
 	public void updateMediaObjectInfo(MediaObject updateMe) {
-		// TODO Auto-generated method stub
+		MP3File tagFile;
+		try {
+			tagFile = new MP3File(updateMe.getLocation());
+			if (tagFile.hasID3v2Tag())
+				_mp3Info = tagFile.getID3v2Tag();
+			else
+				_mp3Info = tagFile.getID3v1Tag();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TagException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ReadOnlyFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAudioFrameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		updateMe.setArtist(_mp3Info.getFirstArtist());
+		updateMe.setTitle(_mp3Info.getFirstTitle());
+		updateMe.setAlbum(_mp3Info.getFirstAlbum());
+		updateMe.setYear(Integer.parseInt(_mp3Info.getFirstYear()));
 	}
 }
